bb355d5b37193444bda48a34e104b3541138c4af
Sharon Bello <sharonbellotech@gmail.com>
2023-03-05 10:29:52 +0200

    loader

--- src/components/loader.jsx
+++ src/components/loader.jsx
@@ -2,8 +2,13 @@ import ReactLoading from "react-loading"
 
 export const Loader = () => {
     return (
-        <div className="loader">
-            <ReactLoading type={"spinningBubbles"} color="#1DBF73" height={'20%'} width={'20%'} />
+        <div className="loader" style={{
+                position: "absolute",
+                top: "50%",
+                left: "50%",
+                translate: "transform(-50%, -50%)",
+        }}>
+            <ReactLoading type={"spin"} color="#C9DB39" height={'20%'} width={'20%'} />
         </div>
     )
 }

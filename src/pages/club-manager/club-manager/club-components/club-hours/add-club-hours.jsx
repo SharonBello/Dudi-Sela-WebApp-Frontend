d95eb1682b82721272af1451ee5479b54e29b267
sharonbellotech@gmail.com <doronkabaso@gmail.com>
2023-05-23 17:20:03 +0300

    multi select club hours days

--- src/pages/club-manager/club-manager/club-components/club-hours/add-club-hours.jsx
+++ src/pages/club-manager/club-manager/club-components/club-hours/add-club-hours.jsx
@@ -5,10 +5,10 @@ import { WeekDays, DayHours, DemoWorkHours, HourConstraint } from '../../club-he
 import Button from '@mui/material/Button';
 
 export const AddClubHours = ({handleSaveClubHours}) => {
-  const [workDays, setWorkDays] = useState(WeekDays);
   const [workHours, setWorkHours] = useState([]);
   const [fromHour, setFromHour] = useState('06:00');
   const [tillHour, setTillHour] = useState('11:00');
+  const [validForDays, setValidForDays] = useState([])
 
   useEffect(() => {
     if (workHours.length === 0)
@@ -16,10 +16,10 @@ export const AddClubHours = ({handleSaveClubHours}) => {
   }, [])
   return (
     <Box className="club-hours-fields-container flex justify-between">
-        <SelectMenu multiple={true} inputLabel="ימים" defaultValue={[]} values={workDays} setValue={setWorkDays} />
+        <SelectMenu multiple={true} inputLabel="ימים"     defaultValue={validForDays}    values={WeekDays}    setValue={setValidForDays} />
         <SelectMenu inputLabel="משעה" defaultValue={HourConstraint.hours.startHour} values={DayHours()} setValue={setFromHour} />
         <SelectMenu inputLabel="עד שעה" defaultValue={HourConstraint.hours.endHour} values={DayHours()} setValue={setTillHour} />
-        <Button variant="contained" component="label" onClick={(e) => handleSaveClubHours(e, {"hours": {"startHour": fromHour, "endHour": tillHour}, "days": workDays})}>שמור</Button>
+        <Button variant="contained" component="label" onClick={(e) => handleSaveClubHours(e, {"hours": {"startHour": fromHour, "endHour": tillHour}, "days": validForDays})}>שמור</Button>
     </Box>
   )
 }
